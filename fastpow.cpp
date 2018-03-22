494411425624209e5aa2dee4417ebb5b6658b8ab
Noszály Áron <noszalyaron4@gmail.com>
2018-03-22 14:31:50 +0100

    bugfix

--- fastpow.cpp
+++ fastpow.cpp
@@ -16,7 +16,7 @@ template<typename T>
 T fastpow(T x, int a, T mod) {
 	if(a==1) return x;
 	
-	T fele=fastpow(x, a/2);
+	T fele=fastpow(x, a/2, mod);
 	T ans=(fele*fele)%mod;
 	
 	if(a&1) ans=(ans*x)%mod;
