72cf1188ed439916a3aacfcaad23b8736f3ce8c5
Noszály Áron <noszalyaron4@gmail.com>
2018-03-23 11:07:48 +0100

    add z algorithm

---
+++ zalgo.cpp
@@ -0,0 +1,23 @@
+vector<int> zalgo(string& str) {
+	vector<int> z(sz(str));
+	int L=0, R=0;
+	for(int i=1;i<sz(str);++i) {
+		if(i>R) {
+			L=i;R=i;
+			while(R<sz(str) && str[R]==str[R-L]) R++;
+			z[i]=R-L;
+			R--;
+		}else {
+			if(z[i-L]+i<R) {
+				z[i]=z[i-L];
+			}else {
+				L=i;
+				while(R<sz(str) && str[R]==str[R-L]) R++;
+				z[i]=R-L;
+				R--;
+			}
+		}
+	}
+	
+	return z;
+}
