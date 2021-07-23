f3d2b2b017ef91683bc235a608c8b81021ac17e9
Áron Noszály <noszalyaron4@gmail.com>
2021-07-23 11:48:49 +0200

    math/sieve add more useful functions

--- src/math/sieve.hpp
+++ src/math/sieve.hpp
@@ -1,6 +1,6 @@
 #include "../template.hpp"
 
-const int MAXN = 1e6+1;
+const int MAXN = 1e6+10;
 int lp[MAXN];
 vector<int> ps;
 
@@ -15,4 +15,36 @@ void sieve() {
 			lp[i*ps[j]]=ps[j];
 		}
 	}
+}
+
+vector<pair<int,int>> pfactors(int x) {
+	vector<pair<int,int>> res;
+	while(x>1) {
+		int p=lp[x];
+		res.push_back({p, 0});
+		while(x%p==0) x/=p, res.back().yy++;
+	}
+	return res;
+}
+
+
+void divs_gen(int ind, int div, vector<pair<int,int>>& pfacts, vector<int>& res) {
+	if(ind==sz(pfacts)) {
+		res.push_back(div);
+		return ;
+	}
+	
+	divs_gen(ind+1, div, pfacts, res);
+	for(int i=0;i<pfacts[ind].yy;++i) {
+		div*=pfacts[ind].xx;
+		divs_gen(ind+1, div, pfacts, res);
+	}
+}
+
+vector<int> divs(int x) {
+	vector<int> res;
+	vector<pair<int,int>> pfacts=pfactors(x);
+	divs_gen(0, 1, pfacts, res);
+
+	return res;
 }
--- src/math/tests/sieve_tests.hpp
+++ src/math/tests/sieve_tests.hpp
@@ -18,6 +18,41 @@ TEST_CASE("sieve tests") {
     REQUIRE(ps[9]==29);
     REQUIRE(ps[10]==31);
 
-    REQUIRE(MAXN==1000001);
-    REQUIRE(ps.size()==78498);
+    REQUIRE(MAXN==1000010);
+    REQUIRE(ps.size()==78499);
+}
+
+TEST_CASE("pfactors tests") {
+    sieve();
+
+    REQUIRE(pfactors(10).size()==2);
+    REQUIRE(pfactors(2).size()==1);
+    
+    auto factors=pfactors(12);
+    sort(factors.begin(), factors.end());
+    REQUIRE(factors==vector<pair<int,int>>({{2,2},{3,1}}));
+}
+
+TEST_CASE("divs tests") {
+    sieve();
+    vector<pair<int, vector<int>>> tests = {
+      {1,{1}},  
+      {2,{1,2}},
+      {3,{1,3}},
+      {4,{1,2,4}},
+      {5,{1,5}},
+      {6,{1,2,3,6}},
+      {7,{1,7}},
+      {8,{1,2,4,8}},
+      {9,{1,3,9}},
+      {10,{1,2,5,10}},
+      {144,{1,2,3,4,6,8,9,12,16,18,24,36,48,72,144}},
+    };
+
+    for(auto test:tests) {
+        auto got=divs(test.xx);
+        sort(all(got));
+
+        REQUIRE(got==test.yy);
+    }
 }
